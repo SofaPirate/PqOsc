1992cb3cbca899b303aeb6b0c092f583d4950440
Sofian Audry <info@sofianaudry.com>
2026-01-25 15:49:10 -0500

    In-depth cleanup and comment documentation.

--- src/PqOSC.cpp
+++ src/PqOSC.cpp
@@ -2,7 +2,7 @@
  * PqOsc.cpp
  *
  * (c) 2025 Thomas O Fredericks :: tof(@)t-o-f(.)info
- * (c) 2026 Sofian Audry        :: info(@)sofianaudry(.)com
+ * (c) 2025 Sofian Audry        :: info(@)sofianaudry(.)com
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,134 +21,202 @@
 
 namespace pq {
 
+// Static singleton list of all OscIn instances.
 HybridArrayList<OscIn*, PLAQUETTE_MAX_UNITS>& OscIn::oscInList() {
   static HybridArrayList<OscIn*, PLAQUETTE_MAX_UNITS> instance;
   return instance;
 }
 
+OscIn::OscIn(MicroOsc& osc, const char *address, Engine& engine)
+    : Unit(engine), _microOsc(osc), _address(address), _value(0), _valueUpdated(false)
+{
+  oscInList().add(this);
+}
+
+void OscIn::begin() {
+  _messageReceived = _valueUpdated = false;
+}
+
+void OscIn::step() {
+  _valueUpdated = _messageReceived;
+  _messageReceived = false;
+}
+
+bool OscIn::eventTriggered(EventType eventType) {
+  switch (eventType) {
+    case EVENT_UPDATE: return updated();
+    default:           return Unit::eventTriggered(eventType);
+  }
+}
+
+void OscIn::receive(float value) {
+  _value = value;
+  _messageReceived = true;
+}
+
+const char* OscIn::address() const {
+  return _address;
+}
+
+float OscIn::get() {
+  return _value;
+}
+
+bool OscIn::updated() const {
+  return _valueUpdated;
+}
+
+void OscIn::onUpdate(EventCallback callback) {
+  onEvent(callback, EVENT_UPDATE);
+}
+
+float OscIn::put(float value) {
+  // Disabled: OscIn is input-only.
+  return value;
+}
+
 void OscIn::handleOSCMessageCallback(MicroOscMessage &message)
 {
-    HybridArrayList<OscIn*, PLAQUETTE_MAX_UNITS>& oscInputs = oscInList();
-    for (size_t i = 0; i < oscInputs.size(); i++)
+  HybridArrayList<OscIn*, PLAQUETTE_MAX_UNITS>& oscInputs = oscInList();
+  for (size_t i = 0; i < oscInputs.size(); i++)
+  {
+    OscIn *oscIn = oscInputs[i];
+
+    // Check if message matches this OscIn's source and address.
+    if (message.checkSource(&oscIn->_microOsc) && message.checkOscAddress(oscIn->address()))
     {
-        OscIn *oscIn = oscInputs[i];
+      // Get type tag.
+      char typeTag;
+      message.copyTypeTags(&typeTag, sizeof(typeTag));
 
-        if (message.checkSource(&oscIn->_microOsc) && message.checkOscAddress(oscIn->address()))
-        {
-            // Get typetag.
-            char typeTag;
-            message.copyTypeTags(&typeTag, sizeof(typeTag));
+      // Convert OSC value to float based on type tag.
+      float value = 0;
+      switch (typeTag) {
+        case 'f': // float
+          value = message.nextAsFloat();
+          break;
 
-            // Get value.
-            float value = 0;
-            switch (typeTag) {
-                case 'f': {
-                    value = message.nextAsFloat();
-                    break;
-                }
-                case 'd': {
-                    value = static_cast<float>(message.nextAsDouble());
-                    break;
-                }
-                case 'i': {
-                    value = static_cast<float>(message.nextAsInt());
-                    break;
-                }
-                case 'b': {
-                    const unsigned char *blobData = 0;
-                    if (message.nextAsBlob(&blobData) == sizeof(float)) {
-                        memcpy(&value, blobData, sizeof(float));
-                    }
-                    break;
-                }
-                case 's': {
-                    const char* str = message.nextAsString();
-                    value = atof(str);
-                    break;
-                }
+        case 'd': // double
+          value = static_cast<float>(message.nextAsDouble());
+          break;
 
-                case 'N':
-                case 'I':
-                case 'T': {
-                    value = 1;
-                    break;
-                }
+        case 'i': // int32
+          value = static_cast<float>(message.nextAsInt());
+          break;
 
-                case 'F': {
-                    value = 0;
-                    break;
-                }
-
-                // Unsupported.
-                case 't': // osc timetag
-                case 'm':
-                case 'h':
-                default:
-                    value = 0;
-            }
-
-            // Set value.
-            oscIn->receive(value);
+        case 'b': { // blob (extract float if size matches)
+          const unsigned char *blobData = 0;
+          if (message.nextAsBlob(&blobData) == sizeof(float)) {
+            memcpy(&value, blobData, sizeof(float));
+          }
+          break;
         }
+
+        case 's': // string (parse as float)
+          value = atof(message.nextAsString());
+          break;
+
+        case 'N': // nil
+        case 'I': // impulse
+        case 'T': // true
+          value = 1;
+          break;
+
+        case 'F': // false
+          value = 0;
+          break;
+
+        // Unsupported types.
+        case 't': // OSC timetag
+        case 'm': // MIDI message
+        case 'h': // int64
+        default:
+          value = 0;
+      }
+
+      // Send value to the OscIn instance.
+      oscIn->receive(value);
     }
+  }
+}
+
+OscOut::OscOut(MicroOsc& osc, const char *address, Engine& engine)
+    : OscOut(osc, address, 'f', engine)
+{
+}
+
+OscOut::OscOut(MicroOsc& osc, const char *address, char typeTag, Engine& engine)
+    : Unit(engine), _microOsc(osc), _address(address), _value(0), _typeTag(typeTag), _needToSend(false)
+{
+}
+
+float OscOut::put(float value) {
+  _value = value;
+  _needToSend = true;
+  return get();
+}
+
+float OscOut::get() {
+  return _value;
+}
+
+void OscOut::step() {
+  if (_needToSend) {
+    _sendMessage();
+    _needToSend = false;
+  }
 }
 
 void OscOut::_sendMessage() {
+  switch (_typeTag) {
+    // Value types.
+    case 'f': // float
+      _microOsc.sendFloat(_address, _value);
+      break;
 
-    switch (_typeTag) {
+    case 'd': // double
+      _microOsc.sendDouble(_address, _value);
+      break;
 
-        // Values.
-        case 'f': {
-            _microOsc.sendFloat(_address, _value); 
-            break;
-        }
-        case 'd': {
-            _microOsc.sendDouble(_address, _value); 
-            break;
-        }
-        case 'i': {
-            _microOsc.sendInt(_address, round(_value)); 
-            break;
-        }
-        case 'b': {
-            _microOsc.sendBlob(_address, (unsigned char *) &_value, sizeof(_value)); 
-            break;
-        }
-        case 's': {
-            char str[32];
-            sprintf(str, "%f", _value);
-            _microOsc.sendString(_address, str);
-            break;
-        }
+    case 'i': // int32
+      _microOsc.sendInt(_address, round(_value));
+      break;
 
-        // Triggers.
-        case 'T': { // true
-            _microOsc.sendTrue(_address); 
-            break;
-        }
-        case 'F': { // false
-            _microOsc.sendFalse(_address); 
-            break;
-        }
-        case 'N': { // nil
-            _microOsc.sendNull(_address); 
-            break;
-        }
-        case 'I': { // impulse
-            _microOsc.sendImpulse(_address); 
-            break;
-        }
+    case 'b': // blob
+      _microOsc.sendBlob(_address, (unsigned char *) &_value, sizeof(_value));
+      break;
 
-        // Unsupported.
-        case 't': // osc timetag
-        case 'm':
-        case 'h':
-        default:;
+    case 's': { // string
+      char str[32];
+      sprintf(str, "%f", _value);
+      _microOsc.sendString(_address, str);
+      break;
     }
 
-    // if (!isValue) {
-    //     _value = 0.0;
-    // }
+    // Trigger types.
+    case 'T': // true
+      _microOsc.sendTrue(_address);
+      break;
+
+    case 'F': // false
+      _microOsc.sendFalse(_address);
+      break;
+
+    case 'N': // nil
+      _microOsc.sendNull(_address);
+      break;
+
+    case 'I': // impulse
+      _microOsc.sendImpulse(_address);
+      break;
+
+    // Unsupported types.
+    case 't': // OSC timetag
+    case 'm': // MIDI message
+    case 'h': // int64
+    default:
+      break;
+  }
 }
 
 }
--- src/PqOSC.h
+++ src/PqOSC.h
@@ -2,7 +2,7 @@
  * PqOsc.h
  *
  * (c) 2025 Thomas O Fredericks :: tof(@)t-o-f(.)info
- * (c) 2026 Sofian Audry        :: info(@)sofianaudry(.)com
+ * (c) 2025 Sofian Audry        :: info(@)sofianaudry(.)com
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -27,222 +27,295 @@
 
 namespace pq
 {
-    // ===============================================================
-    class OscIn; // forward declaration
 
-    // OscIn class ----------------------------------------------------------- |
-    class OscIn : public Unit
+class OscIn; // forward declaration
+
+/**
+ * Receives OSC messages on a specific address.
+ *
+ * OscIn listens for incoming OSC messages matching its address and converts
+ * the received value to a float. Use updated() to check for new messages
+ * or onUpdate() for event-driven handling.
+ */
+class OscIn : public Unit
+{
+protected:
+  /// Shared static container for all OscIn instances (for message routing).
+  static HybridArrayList<OscIn*, PLAQUETTE_MAX_UNITS>& oscInList();
+
+  // The OSC address to listen on.
+  const char *_address;
+
+  // The last received value.
+  float _value;
+
+  // Reference to the OSC transport layer.
+  MicroOsc& _microOsc;
+
+  // Flag indicating a message was received this frame.
+  bool _messageReceived : 1;
+
+  // Flag indicating value was updated last step.
+  bool _valueUpdated : 1;
+
+  // Reserved for future use.
+  uint8_t _unused : 6;
+
+protected:
+  virtual void begin() override;
+  virtual void step() override;
+
+  /// Returns true iff an event of a certain type has been triggered.
+  virtual bool eventTriggered(EventType eventType) override;
+
+  /**
+   * Receives a value from an incoming OSC message.
+   * @param value the received value
+   */
+  void receive(float value);
+
+public:
+  /**
+   * Constructor.
+   * @param osc reference to the OSC transport (OscSlip or OscUdp)
+   * @param address the OSC address to listen on (e.g., "/sensor")
+   * @param engine the engine running this unit
+   */
+  OscIn(MicroOsc& osc, const char *address, Engine& engine = Engine::primary());
+
+  /// Returns the OSC address.
+  const char *address() const;
+
+  /// Returns the current value.
+  virtual float get() override;
+
+  /// Returns true iff a new value was received last step.
+  virtual bool updated() const;
+
+  /// Registers callback for when a new value is received.
+  virtual void onUpdate(EventCallback callback);
+
+  /**
+   * Static callback for handling incoming OSC messages.
+   * Routes messages to matching OscIn instances.
+   * @param message the received OSC message
+   */
+  static void handleOSCMessageCallback(MicroOscMessage &message);
+
+private:
+  // Disabled: OscIn is input-only.
+  virtual float put(float value) override;
+};
+
+/**
+ * OSC transport layer using SLIP encoding over serial.
+ *
+ * OscSlip handles serial communication with SLIP (Serial Line Internet Protocol)
+ * encoding for OSC messages. Use with OscIn and OscOut for bidirectional
+ * OSC communication over a serial connection.
+ *
+ * @tparam MICRO_OSC_IN_SIZE buffer size for incoming messages
+ */
+template <const size_t MICRO_OSC_IN_SIZE>
+class OscSlip : public Unit, public MicroOscSlip<MICRO_OSC_IN_SIZE>
+{
+private:
+  // Pointer to serial port (if managed).
+  SerialType* _serial = nullptr;
+
+  // Baud rate for serial communication.
+  unsigned long _baudRate = 0;
+
+protected:
+  void step() override
+  {
+    this->onOscMessageReceived(OscIn::handleOSCMessageCallback);
+  }
+
+  void begin() override
+  {
+    if (_serial)
     {
-    protected:
-        // Shared static container containing all units. Static because it is shared between all OscIn units.
-        static HybridArrayList<OscIn*, PLAQUETTE_MAX_UNITS>& oscInList();
+      // Safely start serial.
+      beginSerial(*_serial, _baudRate);
+    }
+  }
 
-        const char *_address;
-        float _value;
-        MicroOsc &_microOsc;
-        bool _messageReceived : 1;
-        bool _valueUpdated : 1;
-        uint8_t _unused : 6;
+public:
+  /**
+   * Constructor with existing stream.
+   * @param stream reference to an already initialized stream
+   * @param engine the engine running this unit
+   */
+  OscSlip(Stream& stream, Engine& engine = Engine::primary())
+      : Unit(engine), MicroOscSlip<MICRO_OSC_IN_SIZE>(stream)
+  {
+  }
 
-    protected:
-        void begin() override
-        {
-            _messageReceived = _valueUpdated = false;
-        }
+  /**
+   * Constructor with serial port and baud rate.
+   * @param serial reference to the serial port
+   * @param baudRate baud rate for communication
+   * @param engine the engine running this unit
+   */
+  OscSlip(SerialType& serial, unsigned long baudRate, Engine& engine = Engine::primary())
+      : Unit(engine), MicroOscSlip<MICRO_OSC_IN_SIZE>(serial), _serial(&serial), _baudRate(baudRate)
+  {
+  }
 
-        void step() override {
-            _valueUpdated = _messageReceived;
-            _messageReceived = false;
-        }
+  /**
+   * Constructor with baud rate using default serial.
+   * @param baudRate baud rate for communication
+   * @param engine the engine running this unit
+   */
+  OscSlip(unsigned long baudRate, Engine& engine = Engine::primary())
+      : OscSlip(PLAQUETTE_DEFAULT_SERIAL, baudRate, engine)
+  {
+  }
+};
 
-        /// Returns true iff an event of a certain type has been triggered.
-        bool eventTriggered(EventType eventType) override {
-            switch (eventType) {
-            case EVENT_UPDATE: return updated();
-            default:           return Unit::eventTriggered(eventType);
-            }
-        }    
+/**
+ * OSC transport layer using UDP.
+ *
+ * OscUdp handles network communication for OSC messages over UDP.
+ * Use with OscIn and OscOut for bidirectional OSC communication
+ * over Ethernet or WiFi.
+ *
+ * @tparam MICRO_OSC_IN_SIZE buffer size for incoming messages
+ */
+template <const size_t MICRO_OSC_IN_SIZE>
+class OscUdp : public Unit, public MicroOscUdp<MICRO_OSC_IN_SIZE>
+{
+protected:
+  // UDP port for receiving messages.
+  unsigned int _receivePort = 0;
 
-        void receive(float f)
-        {
-            _value = f;
-            _messageReceived = true;
-        }
+protected:
+  void step() override
+  {
+    this->onOscMessageReceived(OscIn::handleOSCMessageCallback);
+  }
 
-    public:
-        const char *address() const
-        {
-            return _address;
-        }
+  void begin() override
+  {
+    if (_receivePort)
+      this->udp->begin(_receivePort);
+  }
 
-        OscIn(MicroOsc &osc, const char *address, Engine &engine = Engine::primary())
-            : Unit(engine), _microOsc(osc), _address(address), _value(0), _valueUpdated(false)
-        {
-            oscInList().add(this);
-        }
+public:
+  /**
+   * Constructor with UDP instance.
+   * @param udp reference to the UDP instance
+   * @param engine the engine running this unit
+   */
+  OscUdp(UDP& udp, Engine& engine = Engine::primary())
+      : OscUdp(udp, 0, engine)
+  {
+  }
 
-        float get() override
-        {
-            return _value;
-        }
+  /**
+   * Constructor with UDP instance and receive port.
+   * @param udp reference to the UDP instance
+   * @param receivePort port to listen for incoming messages
+   * @param engine the engine running this unit
+   */
+  OscUdp(UDP& udp, unsigned int receivePort, Engine& engine = Engine::primary())
+      : Unit(engine),
+        MicroOscUdp<MICRO_OSC_IN_SIZE>(udp),
+        _receivePort(receivePort)
+  {
+  }
 
-        /// Returns true iff value was changed.
-        virtual bool updated() const { return _valueUpdated; }
+  /**
+   * Constructor with UDP instance and destination.
+   * @param udp reference to the UDP instance
+   * @param destinationIp IP address to send messages to
+   * @param destinationPort port to send messages to
+   * @param engine the engine running this unit
+   */
+  OscUdp(UDP& udp, IPAddress destinationIp, unsigned int destinationPort, Engine& engine = Engine::primary())
+      : OscUdp(udp, 0, destinationIp, destinationPort, engine)
+  {
+  }
 
-        /// Registers event callback on finish event.
-        virtual void onUpdate(EventCallback callback) { onEvent(callback, EVENT_UPDATE); }
+  /**
+   * Constructor with UDP instance, receive port, and destination.
+   * @param udp reference to the UDP instance
+   * @param receivePort port to listen for incoming messages
+   * @param destinationIp IP address to send messages to
+   * @param destinationPort port to send messages to
+   * @param engine the engine running this unit
+   */
+  OscUdp(UDP& udp, unsigned int receivePort, IPAddress destinationIp, unsigned int destinationPort, Engine& engine = Engine::primary())
+      : Unit(engine),
+        MicroOscUdp<MICRO_OSC_IN_SIZE>(udp, destinationIp, destinationPort),
+        _receivePort(receivePort)
+  {
+  }
+};
 
-        // ----------------------------------------------------------------------- |
-        // handleOSCMessageCallback ---------------------------------------------- |
-        static void handleOSCMessageCallback(MicroOscMessage &message);
-        // ---------------------------------------------------------------------- |
+/**
+ * Sends OSC messages to a specific address.
+ *
+ * OscOut queues values received via put() or the flow operator (>>)
+ * and sends them as OSC messages during step(). Supports multiple
+ * OSC type tags for different data formats.
+ */
+class OscOut : public Unit
+{
+protected:
+  // Reference to the OSC transport layer.
+  MicroOsc& _microOsc;
 
-    private:
-        // DISABLE THE PUT
-        float put(float f) override
-        {
-            //_value = f;
-            return f;
-        }
+  // The OSC address to send to.
+  const char *_address;
 
-    };
+  // The value to send.
+  float _value;
 
-    // OscSlip class -------------------------------------------------------- |
-    template <const size_t MICRO_OSC_IN_SIZE>
-    class OscSlip : public Unit, public MicroOscSlip<MICRO_OSC_IN_SIZE>
-    {
-    private:
-        SerialType* _serial = nullptr;
-        unsigned long _baudRate = 0;
+  // OSC type tag ('f', 'i', 'd', etc.).
+  char _typeTag;
 
-    protected:
-        void step() override
-        {
-            this->onOscMessageReceived(OscIn::handleOSCMessageCallback);
-        }
+  // Flag indicating a message needs to be sent.
+  bool _needToSend;
 
-        void begin() override
-        {
-            if (_serial)
-            {
-                // Safely start serial.
-                beginSerial(*_serial, _baudRate);
-            }
-        }
+public:
+  /**
+   * Constructor with default float type.
+   * @param osc reference to the OSC transport (OscSlip or OscUdp)
+   * @param address the OSC address to send to (e.g., "/led")
+   * @param engine the engine running this unit
+   */
+  OscOut(MicroOsc& osc, const char *address, Engine& engine = Engine::primary());
 
-    public:
-        // Constructor with default value for _iic_address
-        OscSlip(Stream &stream, Engine &engine = Engine::primary())
-            : Unit(engine), MicroOscSlip<MICRO_OSC_IN_SIZE>(stream)
-        {
-        }
+  /**
+   * Constructor with specified type tag.
+   * @param osc reference to the OSC transport (OscSlip or OscUdp)
+   * @param address the OSC address to send to (e.g., "/led")
+   * @param typeTag OSC type tag: 'f' (float), 'i' (int), 'd' (double),
+   *                'b' (blob), 's' (string), 'T' (true), 'F' (false),
+   *                'N' (nil), 'I' (impulse)
+   * @param engine the engine running this unit
+   */
+  OscOut(MicroOsc& osc, const char *address, char typeTag, Engine& engine = Engine::primary());
 
-        OscSlip(SerialType &serial, unsigned long baudRate, Engine &engine = Engine::primary())
-            : Unit(engine), MicroOscSlip<MICRO_OSC_IN_SIZE>(serial), _serial(&serial), _baudRate(baudRate)
-        {
-        }
+  /**
+   * Queues a value to be sent as an OSC message.
+   * @param value the value to send
+   * @return the current value
+   */
+  virtual float put(float value) override;
 
-        OscSlip(unsigned long baudRate, Engine &engine = Engine::primary())
-            : OscSlip(PLAQUETTE_DEFAULT_SERIAL, baudRate, engine)
-        {
-        }
+  /// Returns the current value.
+  virtual float get() override;
 
-    };
-    // ----------------------------------------------------------------------- |
+protected:
+  virtual void step() override;
 
-    // OscUdp class ---------------------------------------------------------- |
-    // OSC UDP Communication
-    template <const size_t MICRO_OSC_IN_SIZE>
-    class OscUdp : public Unit, public MicroOscUdp<MICRO_OSC_IN_SIZE>
-    {
-    protected:
-        unsigned int _receivePort = 0;
+  /// Sends the queued OSC message.
+  void _sendMessage();
+};
 
-    protected:
-        void step() override
-        {
-            this->onOscMessageReceived(OscIn::handleOSCMessageCallback);
-        }
-
-        void begin() override
-        {
-            if (_receivePort)
-               this->udp->begin(_receivePort);
-        }
-
-    public:
-        OscUdp(UDP &udp, Engine &engine = Engine::primary())
-            : OscUdp(udp, 0, engine)
-        {
-        }
-
-        OscUdp(UDP &udp, unsigned int receivePort, Engine &engine = Engine::primary())
-            : Unit(engine), 
-              MicroOscUdp<MICRO_OSC_IN_SIZE>(udp), 
-              _receivePort(receivePort)
-        {
-        }
-
-        OscUdp(UDP &udp, IPAddress destinationIp, unsigned int destinationPort, Engine &engine = Engine::primary())
-            : OscUdp(udp, 0, destinationIp, destinationPort, engine)
-        {
-        }
-
-        OscUdp(UDP &udp, unsigned int receivePort, IPAddress destinationIp, unsigned int destinationPort, Engine &engine = Engine::primary())
-            : Unit(engine), 
-              MicroOscUdp<MICRO_OSC_IN_SIZE>(udp, destinationIp, destinationPort),
-              _receivePort(receivePort)
-        {
-        }
-    };
-    // ----------------------------------------------------------------------- |
-
-    // OscOut class ---------------------------------------------------------- |
-    class OscOut : public Unit
-    {
-    protected:
-        MicroOsc &_microOsc;
-        const char *_address;
-        float _value;
-
-        char _typeTag;
-
-        bool _needToSend;
-
-    public:
-        OscOut(MicroOsc &osc, const char *address, Engine& engine = Engine::primary()) 
-            : OscOut(osc, address, 'f', engine)
-        {}
-
-        OscOut(MicroOsc &osc, const char *address, char typeTag, Engine& engine = Engine::primary())
-            : Unit(engine), _microOsc(osc), _address(address), _value(0), _typeTag(typeTag), _needToSend(false)
-        {}
-
-        float put(float f) override
-        {
-            _value = f;
-            _needToSend = true;
-            return get();
-        }
-
-        float get() override
-        {
-            return _value;
-        }
-
-    protected:
-        void step() override
-        {
-            if (_needToSend)
-            {
-                _sendMessage();
-                _needToSend = false;
-            }
-        }
-
-        void _sendMessage();
-
-    };
-    // ----------------------------------------------------------------------- |
 }
+
 #endif
